e0ae6d6ed03504d74600b05bd31cbb239f45cfe9
Alex Gates-Shannon <alex.nebula99@gmail.com>
2026-04-13 14:21:00 -0400

    Add typography styles

--- apps/zoltar-fe/src/main.ts
+++ apps/zoltar-fe/src/main.ts
@@ -1,5 +1,9 @@
 import { mount } from 'svelte';
 
+import './themes/base.css';
+import './themes/mothership.css';
+import './lib/styles/typography.css';
+
 import App from './App.svelte';
 
 const app = mount(App, { target: document.getElementById('app')! });
